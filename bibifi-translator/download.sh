307106bca7270d6216ef439ab1f69cd456220ae7
jprider63 <jp@jamesparker.me>
2016-12-29 14:55:45 -0500

    pending change to file download script

--- bibifi-translator/download.sh
+++ bibifi-translator/download.sh
@@ -7,5 +7,5 @@ export CSV=$1
 # mkdir resumes;
 # ./dist/build/translator/translator retrieve resumes > resumes/resumes.csv;
 
-cat $CSV | sed s/,.*/\ $SESSION/g | awk '{print "curl --cookie \"_SESSION="$2"\" https://www.builditbreakit.org/download/"$1" -J -L -O; mv *.pdf resumes/"$1".pdf; mv *.png resumes/"$1".png; mv *.jpg resumes/"$1".jpg; mv *.jpeg resumes/"$1".jpeg; mv *.docx resumes/"$1".docx; mv *.doc resumes/"$1".doc; mv *.txt resumes/"$1".txt; " }' | /bin/bash
+cat $CSV | sed s/,.*/\ $SESSION/g | awk '{print "curl -k --cookie \"_SESSION="$2"\" https://www.builditbreakit.org/download/"$1" -J -L -O; mv *.pdf resumes/"$1".pdf; mv *.png resumes/"$1".png; mv *.jpg resumes/"$1".jpg; mv *.jpeg resumes/"$1".jpeg; mv *.docx resumes/"$1".docx; mv *.doc resumes/"$1".doc; mv *.txt resumes/"$1".txt; " }' | /bin/bash
 #cat resumes/resumes.csv | sed s/,.*//g | awk '{print "open https://www.builditbreakit.org/download/"$1}'
