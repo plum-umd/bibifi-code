3f02e28379f2c1cc8add9205b99d3ee78045d6a7
Phil Nguyen <pcn@cs.umd.edu>
2019-12-13 00:07:19 +0000

    also insert BreakFixSubmission entry when handling break submissions handle errors when JSON file is invalid and when there's no valid build to break

--- bibifi-webapp/Handler/Webhook/Gitlab/Push.hs
+++ bibifi-webapp/Handler/Webhook/Gitlab/Push.hs
@@ -4,6 +4,8 @@ import PostDependencyType
     ( BuildSubmissionStatus(..)
     , BreakSubmissionStatus(..)
     , FixSubmissionStatus(..)
+    , BreakSubmissionResult(..)
+    , FixSubmissionResult(..)
     )
 import Import as I
 import Data.Aeson as J
@@ -11,6 +13,7 @@ import Data.List (isInfixOf)
 import qualified Data.ByteString.Lazy as BS
 import System.FilePath.Posix as F (splitPath)
 import Data.Text as T (pack)
+import Database.Persist.Sql (toSqlKey)
 --import GitLab as G
 --import GitLab.Types as GT
 --import GitLab.API.Projects as GP
@@ -21,10 +24,10 @@ postWebhookGitlabPushR :: TeamContestId -> Text -> Handler ()
 -- We only treat the call as a ping, and check back with Gitlab to retrieve information
 -- regarding the commit hash.
 postWebhookGitlabPushR tcId token = runLHandler $ do
-    -- FIXME: check that token matches, and using constant time comparison
     pushTime <- getCurrentTime
     contest <- runDB $ do
-        TeamContest _ cId _ _ _ _ <- get404 tcId
+        TeamContest _ cId _ _ _ nonce <- get404 tcId
+        -- FIXME compare against token
         get404 cId
     PushMsg pId cmts <- requireJsonBody
     mapM_ (handleCommit pushTime pId contest tcId) cmts
@@ -33,41 +36,64 @@ handleCommit :: UTCTime -> Int -> Contest -> TeamContestId -> Commit -> LHandler
 -- Insert a submission of the right round in the database for given team and commit-hash
 handleCommit t pId (Contest _ _ bld0 bld1 brk0 brk1) tcId (Commit h added modified)
     | t ∈ (bld0, bld1) =
-          insertDB_ $ BuildSubmission tcId t h BuildPending Nothing Nothing
+          runDB $ insert_ $ BuildSubmission tcId t h BuildPending Nothing Nothing
     | t ∈ (brk0, brk1) = do
           -- Invalidate outdated break submissions then insert new entries
           -- Added tests are treated the same as modified tests to account for
           -- people deleting then adding them back.
           forM_ (addedTests ++ modifiedTests) $ \(f, name) -> do
               runDB $ do
-                  breaks <- selectList [BreakSubmissionName ==. pack name] []
+                  breaks <- selectList [BreakSubmissionName ==. name] []
                   forM_ breaks $ \(Entity id _) -> do
                       update id [ BreakSubmissionValid =. Just False
                                 , BreakSubmissionMessage =. Just "Break resubmitted" ]
                       updateWhere [ BreakFixSubmissionBreak ==. id ]
                                   [ BreakFixSubmissionStatus =. BreakRejected
                                   , BreakFixSubmissionResult =. Nothing ]
-              BreakMsg _ tId <- parseGitLabJSON J.decode pId f
-              insertDB_ $ BreakSubmission tcId tId t h (pack name) Nothing Nothing Nothing Nothing
+              parsed <- parseGitLabJSON J.decode pId f
+              case parsed of
+                  Just (BreakMsg _ tId) -> runDB $ do
+                      -- TODO validate targetTeam
+                      target' <- getLatestBuildOrFix tId
+                      case target' of
+                          Just target -> do
+                              id <- insert $ BreakSubmission tcId tId t h name Nothing Nothing Nothing Nothing
+                              insert_ $ BreakFixSubmission id target Nothing Nothing BreakPending Nothing
+                          Nothing -> do
+                              id <- insert $ BreakSubmission tcId tId t h name Nothing (Just "Invalid target team") Nothing (Just False)
+                              insert_ $ BreakFixSubmission id Nothing Nothing Nothing BreakRejected (Just BreakFailed)
+                  Nothing -> runDB $ do
+                      id <- insert $ BreakSubmission tcId (toSqlKey 1) t h name Nothing (Just "invalid JSON in test.json") Nothing (Just False)
+                      insert_ $ BreakFixSubmission id Nothing Nothing Nothing BreakRejected (Just BreakFailed)
           -- Handle each change to `build/` as a fix submission
           when buildChanged $ -- TODO: so no explicit fix, no name?
-              insertDB_ $ FixSubmission tcId t h FixPending Nothing "" Nothing Nothing Nothing
+              runDB $ insert_ $ FixSubmission tcId t h FixPending Nothing "" Nothing Nothing Nothing
     | t < bld0 = return () -- TODO: contest not started
     | t < brk0 = return () -- TODO: invalid build
     | t > brk1 = return () -- TODO: invalid break/fix
   where
     (∈) x (lo,hi) = lo <= x && x <= addUTCTime tolerance hi
       where tolerance = 10 * 60
-    insertDB_ s = runDB (insert s) >> return ()
     addedTests    = testNames added
     modifiedTests = testNames modified
     buildChanged = any ("build/" `isInfixOf`) modified
     testNames ps = [(p,n) | (p,Just n) <- zip ps (map testName ps)]
     testName p = case reverse (splitPath p) of
-        "test.json":name:"break/":_ -> Just name
+        "test.json":name:"break/":_ -> Just (pack name)
         _                           -> Nothing
+    getLatestBuildOrFix tId = do
+        latestBuild <- selectFirst [ BuildSubmissionTeam ==. tId ]
+                                   [ Desc BuildSubmissionTimestamp ]
+        latestFix   <- selectFirst [ FixSubmissionTeam ==. tId
+                                   , FixSubmissionResult ==. Just FixFixed]
+                                   [ Desc FixSubmissionTimestamp ]
+        case (latestBuild, latestFix) of
+            (_, Just (Entity id _))                     -> return $ Just $ Just id
+            (Just (Entity _ b), _)
+                | buildSubmissionStatus b == BuildBuilt -> return $ Just $ Nothing
+            _                                           -> return Nothing
 
-parseGitLabJSON :: (BS.ByteString -> Maybe a) -> Int -> String -> LHandler a
+parseGitLabJSON :: (BS.ByteString -> Maybe a) -> Int -> String -> LHandler (Maybe a)
 -- Parse JSON file from GitLab repo at given path
 parseGitLabJSON parse pId fn = undefined {- do
     s <- G.runGitLabConfig config $ do
